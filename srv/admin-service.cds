c95a965797506d8b1aba9b7b398800484a217d06
Ashish Jain <ashish.jain09@sap.com>
2025-09-03 23:39:26 +0530

    Added custom action for submitting an order

--- srv/admin-service.cds
+++ srv/admin-service.cds
@@ -4,4 +4,10 @@ service AdminService{
 
     entity Books as projection on db.Books ;
     entity Authors as projection on db.Authors ;
+
+
+action submitOrder(book : db.Books:ID, quantity : Integer) returns {
+        stock : db.Books:stock
+    };
+    
 }
--- srv/admin-service.js
+++ srv/admin-service.js
@@ -6,7 +6,26 @@ class AdminService extends cds.ApplicationService {
 
         this.before(['CREATE', 'UPDATE'], Authors, this.validateLifeData);
 
+         // Reduce the stock of the ordered book according to the ordered quantity
+        this.on('submitOrder', this.reduceStock);
+
         return super.init();
+    }
+
+    reduceStock(req) {
+        /* !!! This is only a preliminary, incomplete implementation of the submitOrder action. !!!
+           !!! In the next lesson, we will learn how to use queries.                            !!!
+           !!! These will then be used to complete the implementation.                          !!! */
+        const { Books } = this.entities;
+        const { book, quantity } = req.data;
+
+        if (quantity < 1) {
+            return req.error('The quantity must be at least 1.');
+        }
+
+        let stock = 10;
+
+        return { stock };
     }
 
     validateLifeData(req) {
